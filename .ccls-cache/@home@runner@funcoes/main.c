d709e6d6469137b38c2946d6713ea18f48d27e92
gabbsenai <162130956+gabbsenai@users.noreply.github.com>
2024-03-05 12:30:39 +0000

    função02

--- .ccls-cache/@home@runner@funcoes/main.c
+++ .ccls-cache/@home@runner@funcoes/main.c
@@ -9,18 +9,26 @@
   //   dizerBomdia("Teixeira");
   //   dizerBomdia("Xandao protetor da terra");
 
-  void dizerBoatarde (char nome[20]){
-    printf("Boa tarde %s!\n", nome);
+  // void dizerBoatarde (char nome[20]){
+  //   printf("Boa tarde %s!\n", nome);
+  // }
+
+  // int main(void){
+  //   char nome[20];
+  //   printf("Digite seu nome: ");
+  //   scanf("%s", nome);
+  //   dizerBoatarde(nome);
+  //   return 0;
+  // }
+
+  int soma(int a, int b){
+    int resultado = a + b;
+    return resultado;
   }
 
   int main(void){
-    char nome[20];
-    printf("Digite seu nome: ");
-    scanf("%s", nome);
-    dizerBoatarde(nome);
-    return 0;
-  }
-
-
+    int num1 = 6, num2 = 4, result;
+    result = soma(num1, num2);
     
-  
+    printf("%d", result);
+  }
